5df87f612327926ea0094c3dbf74a93ab2760728
TheRhettThompson <rthompsoncareer@gmail.com>
2022-11-22 22:00:57 +0000

    generator rendering

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function domainNameGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let extension = [".com", ".net", ".us", ".io"];
+
+    let allDomains = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let description = 0; description < adj.length; description++) {
+        for (let thing = 0; thing < noun.length; thing++) {
+          for (let address = 0; address < extension.length; address++) {
+            allDomains.push(
+              pronoun[i] + adj[description] + noun[thing] + extension[address]
+            );
+          }
+        }
+      }
+    }
+    return allDomains;
+  }
+  let domainCombo = domainNameGenerator();
+  for (let a = 0; a < domainCombo.length; a++) {
+    document.querySelector("#domain").innerHTML +=
+      "<p>" + domainCombo[a] + "</p>";
+  }
 };
+//use nested for loops
+//for loop, string concatenation
+//console.log("Hello world!");
+//for (var i = 0; i < xs.length; i++) { console.log(xs[i]); }
